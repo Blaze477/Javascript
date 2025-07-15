f3f02fceaf1bf0467a2305a6b3e93ddd6969e0fa
unknown <paulo2fresh@gmail.com>
2025-07-15 22:31:58 +0100

    I initialized the project

--- Index.js
+++ Index.js
@@ -1,5 +1,8 @@
-var Username = "Blaze";
-var a = 2;
-var b = 4;
-var c = a + b;
-console.log(c);
+var Elements = [4, 200, 78, 748, 3.14, 67.93, 34.18, 9.81, true, false, true, false, "uniform", "backpack", "notebook", "wardrobe", {brand:"Nike"}, {level:"beginner"}, {language:"html"}, {food:"indomie"}]
+console.log (Elements)
+
+var VSL = {course1: "basic computer training", course2: "graphics design training", course3: "website design training", course4: "digital marketing training", course5: "social media marketing", course6: "programming training", course7: "UI/UX training", course8: "cybersecurity training", course9: "data analysis training"}
+console.log (VSL)
+
+var  States = {1:17, 2:21, 3:31, 4:21, 5:20, 6:8, 7:23, 8:27, 9:18, 10:25, 11:13, 12:18, 13:16, 14:17, 15:11, 16:27, 17:27, 18:23, 19:44, 20:34, 21:21, 22:21, 23:16, 24:20, 25:13, 26:25, 27:20, 28:18, 29:30, 30:33, 31:17, 32:23, 33:23, 34:16, 35:17 ,36:14}
+console.log (States) 
